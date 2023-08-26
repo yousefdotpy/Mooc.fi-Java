af7e032b20c159fa426ceb79c15fed4390237fca
joeproo <57537303+joeproo@users.noreply.github.com>
2023-08-26 03:13:13 +0300

    Completed the Store

--- mooc-java-programming-ii/part09-Part09_10.OnlineShop/src/main/java/Main.java
+++ mooc-java-programming-ii/part09-Part09_10.OnlineShop/src/main/java/Main.java
@@ -1,16 +1,18 @@
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // Test your app here
         Warehouse warehouse = new Warehouse();
-        warehouse.addProduct("milk", 3, 10);
-        warehouse.addProduct("coffee", 5, 7);
+        warehouse.addProduct("coffee", 5, 10);
+        warehouse.addProduct("milk", 3, 20);
+        warehouse.addProduct("cream", 2, 55);
+        warehouse.addProduct("bread", 7, 8);
 
-        System.out.println("prices:");
-        System.out.println("milk: " + warehouse.price("milk"));
-        System.out.println("coffee: " + warehouse.price("coffee"));
-        System.out.println("sugar: " + warehouse.price("sugar"));
+        Scanner scanner = new Scanner(System.in);
+
+        Store store = new Store(warehouse, scanner);
+        store.shop("John");
     }
 }
