98fe5500d5b64cd1a2a4e941013a0747812e4470
joeproo <57537303+joeproo@users.noreply.github.com>
2023-08-27 03:28:16 +0300

    Producing collection through stream() method

--- mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -8,7 +9,25 @@ public class PositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
+        List<Integer> inputs = new ArrayList<>();
 
+        // reading inputs
+        while (true) {
+            String row = scanner.nextLine();
+            if (row.equals("end")) {
+                break;
+            }
+
+            inputs.add(Integer.valueOf(row));
+        }
+        System.out.println(positive(inputs));
+
+    }
+    public static List<Integer> positive(List<Integer> numbers){
+        List<Integer> positiveList = numbers.stream()
+                .filter(s -> s >0)
+                .collect(Collectors.toList());
+        return positiveList;
     }
 
 }
