f631184f8a848e5db5ada4085632377beb4b6348
joeproo <57537303+joeproo@users.noreply.github.com>
2023-08-27 04:08:33 +0300

    print all the numbers the user has given as input that are between 1 and 5.

--- mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,16 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true){
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input < 0) {
+                break;
+            }
+            list.add(input);
+        }
+        list.stream()
+                .filter(s -> s >= 1 && s <= 5)
+                .forEach(s -> System.out.println(s));
     }
 }
