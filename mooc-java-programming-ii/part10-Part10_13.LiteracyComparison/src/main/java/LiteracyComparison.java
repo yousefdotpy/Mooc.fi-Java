6758637877a26a86f85ab1034792c1c79cd9fe1f
joeproo <57537303+joeproo@users.noreply.github.com>
2023-09-03 17:36:45 +0300

    Lambda expression and comparable

--- mooc-java-programming-ii/part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ mooc-java-programming-ii/part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -1,12 +1,7 @@
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class LiteracyComparison {
     
@@ -16,7 +11,7 @@ public class LiteracyComparison {
             Files.lines(Paths.get("literacy.csv"))
                     .map(x -> x.split(","))
                     .sorted((x, y) -> x[5].compareTo(y[5]))
-                    .forEach(row -> System.out.println(row[3] + " (" + row[4] + "), " + row[2].split(" ")[1].trim() + ", " + row[5]));
+                    .forEach(line -> System.out.println(line[3] + " (" + line[4] + "), " + line[2].split(" ")[1].trim() + ", " + line[5]));
 
         } catch (IOException e) {
             throw new RuntimeException(e);
