7eff121b6b10ae63653907b8273da39a62d17181
joeproo <57537303+joeproo@users.noreply.github.com>
2023-09-06 20:04:22 +0300

    First packages

---
+++ mooc-java-programming-ii/part11-Part11_08.FirstPackages/src/main/java/mooc/logic/ApplicationLogic.java
@@ -0,0 +1,16 @@
+package mooc.logic;
+
+import mooc.ui.UserInterface;
+
+public class ApplicationLogic {
+    private UserInterface ui;
+    public ApplicationLogic(UserInterface ui) {
+        this.ui = ui;
+    }
+    public void execute(int times){
+        for (int i = 0; i < times; i++ ){
+            System.out.println("Application logic is working" );
+            ui.update();
+        }
+    }
+}
---
+++ mooc-java-programming-ii/part11-Part11_08.FirstPackages/src/main/java/mooc/ui/TextInterface.java
@@ -0,0 +1,9 @@
+package mooc.ui;
+
+public class TextInterface implements UserInterface{
+
+    @Override
+    public void update() {
+        System.out.println("Updating UI");
+    }
+}
