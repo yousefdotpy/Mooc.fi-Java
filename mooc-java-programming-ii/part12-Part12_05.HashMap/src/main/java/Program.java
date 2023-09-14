10c9b03b7c6b9963dfdf32e8b5585e0bafb58203
joeproo <57537303+joeproo@users.noreply.github.com>
2023-09-14 20:55:52 +0300

    Implementing HashMap

--- mooc-java-programming-ii/part12-Part12_05.HashMap/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_05.HashMap/src/main/java/Program.java
@@ -1,12 +1,10 @@
-
+import java.util.Random;
 import java.util.Scanner;
-
 public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // You can test the class here
-
     }
 
 }
