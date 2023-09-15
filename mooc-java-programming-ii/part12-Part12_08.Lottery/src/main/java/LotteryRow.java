3c59e0218b90f52cc5bd3f4bd0446f03362c4bf5
joeproo <57537303+joeproo@users.noreply.github.com>
2023-09-15 22:46:15 +0300

    LotteryRow

--- mooc-java-programming-ii/part12-Part12_08.Lottery/src/main/java/LotteryRow.java
+++ mooc-java-programming-ii/part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -17,14 +17,21 @@ public class LotteryRow {
 
     public void randomizeNumbers() {
         // Initialize the list for numbers
+        Random random = new Random();
         this.numbers = new ArrayList<>();
+        while (this.numbers.size() < 7){
+            int number = random.nextInt(40) + 1;
+            if (!this.containsNumber(number)) {
+                this.numbers.add(number);
+            }
+        }
         // Implement the random number generation here
         // the method containsNumber is probably useful
     }
 
     public boolean containsNumber(int number) {
         // Check here whether the number is among the drawn numbers
-        return false;
+        return this.numbers.contains(number);
     }
 }
-
+
