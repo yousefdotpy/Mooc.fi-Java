0864cb591be094ee2b93cb16b69bfe0357706149
joeproo <57537303+joeproo@users.noreply.github.com>
2023-09-15 23:53:00 +0300

    Array As String

--- mooc-java-programming-ii/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -3,6 +3,22 @@ public class Program {
 
     public static void main(String[] args) {
         // Test your method here
+        int rows = 2;
+        int columns = 3;
+        int[][] matrix = new int[rows][columns];
+        matrix[0][1] = 5;
+        matrix[1][0] = 3;
+        matrix[1][2] = 7;
+        System.out.println(arrayAsString(matrix));
     }
-
+    public static String arrayAsString(int[][] array){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int[] x : array) {
+            for (int y : x) {
+                stringBuilder.append(y);
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
 }
