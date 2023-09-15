95f9657af608051f136b0aa6ead790078a5fe03e
joeproo <57537303+joeproo@users.noreply.github.com>
2023-09-16 00:01:13 +0300

    Making a magic square

--- mooc-java-programming-ii/part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
+++ mooc-java-programming-ii/part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
@@ -3,10 +3,30 @@ public class MagicSquareFactory {
 
     public MagicSquare createMagicSquare(int size) {
 
-        MagicSquare square = new MagicSquare(size);
+        //MagicSquare square = new MagicSquare(size);
+        MagicSquare magicSquare = new MagicSquare(size);
 
-        // implement the creation of a magic square with the Siamese method algorithm here
-        return square;
+        int row = 0;
+        int col = size / 2;
+        magicSquare.placeValue(row, col, 1);
+        for (int k = 2; k <= size * size; k++) {
+            int currentRow = row;
+            int currentColumn = col;
+            row -= 1;
+            col += 1;
+            if (row < 0) {
+                row = size - 1;
+            }
+            if (col > size - 1) {
+                col = 0;
+            }
+            if (magicSquare.readValue(row, col) > 0) {
+                row = currentRow + 1;
+                col = currentColumn;
+            }
+            magicSquare.placeValue(row, col, k);
+        }
+        return magicSquare;
     }
 
 }
