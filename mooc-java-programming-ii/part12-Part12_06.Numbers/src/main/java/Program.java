1bda5a00b5fdb329aba35d80298fb03b6fec9a51
joeproo <57537303+joeproo@users.noreply.github.com>
2023-09-15 22:01:29 +0300

    Printing random numbers

--- mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,12 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("How many random numbers should be printed?");
+        int numbers = Integer.parseInt(scanner.next());
+        for (int i = 0; i < numbers; i++) {
+            System.out.println(random.nextInt(11));
+        }
     }
 
 }
